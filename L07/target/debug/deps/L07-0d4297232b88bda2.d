bd2f6bb1d400c322085ad35055cc40252142424e
Diego Torres <diego@infinitelabs.co>
2022-10-03 15:38:22 +0200

    Delete L07/target directory

--- L07/target/debug/deps/L07-0d4297232b88bda2.d
+++
@@ -1,5 +0,0 @@
-/home/diego/Curso Polkadot/Curso-Rust/L07/target/debug/deps/L07-0d4297232b88bda2.rmeta: src/main.rs
-
-/home/diego/Curso Polkadot/Curso-Rust/L07/target/debug/deps/L07-0d4297232b88bda2.d: src/main.rs
-
-src/main.rs:
--- L07/target/debug/deps/traits2-e15ce1bf0f6198f8.d
+++
@@ -1,5 +0,0 @@
-/home/diego/Curso Polkadot/Curso-Rust/L07/target/debug/deps/traits2-e15ce1bf0f6198f8.rmeta: tests/traits2.rs
-
-/home/diego/Curso Polkadot/Curso-Rust/L07/target/debug/deps/traits2-e15ce1bf0f6198f8.d: tests/traits2.rs
-
-tests/traits2.rs:
