250858974d0434598f65106a4f08c77f08c35173
Diego Torres <diego@infinitelabs.co>
2022-10-03 15:38:11 +0200

    Delete L06/target directory

--- L06/target/debug/deps/modules1-9f899abb1d8a323b.d
+++
@@ -1,5 +0,0 @@
-/home/diego/Curso Polkadot/Curso-Rust/L06/target/debug/deps/modules1-9f899abb1d8a323b.rmeta: tests/modules1.rs
-
-/home/diego/Curso Polkadot/Curso-Rust/L06/target/debug/deps/modules1-9f899abb1d8a323b.d: tests/modules1.rs
-
-tests/modules1.rs:
