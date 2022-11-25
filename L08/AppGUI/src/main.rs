fa1df3ac2d3120238b63554a12eb540ed4051a14
diegotorreslopez81 <diego@infinitelabs.co>
2022-11-25 17:56:46 +0100

    update project

--- L08/AppGUI/src/main.rs
+++ L08/AppGUI/src/main.rs
@@ -1,12 +1,11 @@
 // use entidad::Vivienda;
-
-use crate::entidad::ViviendaDAO;
+// use crate::entidad::ViviendaDAO;
 
 mod entidad;
 mod presentacion;
 
 fn main() {
-    let mut viviendaDAO = ViviendaDAO::new();
+    // let viviendaDAO = ViviendaDAO::new();
     let mut gui = presentacion::GUI::new();
     gui.build();
     gui.show();    
